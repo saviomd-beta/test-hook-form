7a8c9172b9b9e7e416210e70b5952d10a68e47e9
Sávio Mendes <saviomd@users.noreply.github.com>
2024-05-31 17:53:50 -0300

    Add form with basic fields and config

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,47 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useForm, SubmitHandler } from "react-hook-form";
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+enum GenderEnum {
+  f = "female",
+  m = "male",
+  o = "other",
 }
 
-export default App
+interface IFormInput {
+  firstName: string;
+  lastName: string;
+  gender?: GenderEnum;
+}
+
+function App() {
+  const { register, handleSubmit } = useForm<IFormInput>();
+
+  const onSubmit: SubmitHandler<IFormInput> = (data) => console.log(data);
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div>
+        <label htmlFor="firstName">first name</label>
+        <br />
+        <input {...register("firstName")} type="text" />
+      </div>
+      <div>
+        <label htmlFor="lastName">last name</label>
+        <br />
+        <input {...register("lastName")} type="text" />
+      </div>
+      <div>
+        <label htmlFor="gender">gender</label>
+        <br />
+        <select {...register("gender")}>
+          <option>--</option>
+          <option value="f">female</option>
+          <option value="m">male</option>
+          <option value="o">other</option>
+        </select>
+      </div>
+      <button type="submit">send</button>
+    </form>
+  );
+}
+
+export default App;
